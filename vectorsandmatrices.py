08722648ac13ea146c9966be9538c2c7d0754ebf
twheelertech <tony@wheelertechconsulting.com>
2019-09-02 18:22:22 -0500

    Added Vectors and Matrices

---
+++ vectorsandmatrices.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 18:13:10 2019
+
+@author: tony
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+M = np.array([ [1,2], [3,4] ])
+
+L = [ [1,2], [3,4] ]
+
+L[0]
+
+L[0][0]
+
+
+M[0][0]
+
+M[0,0]
+
+M2 = np.matrix([ [1,2], [3,4] ])
+
+M2
+
+#Convert Matrix to np.array
+A = np.array(M2)
+
+A
