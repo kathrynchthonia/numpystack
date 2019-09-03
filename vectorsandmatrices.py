a2260152014eda5b346a4834f38c1f69b9f1b5e3
twheelertech <tony@wheelertechconsulting.com>
2019-09-03 00:36:02 -0500

    More Vectors and Matrices

--- vectorsandmatrices.py
+++ vectorsandmatrices.py
@@ -29,3 +29,73 @@ M2
 A = np.array(M2)
 
 A
+
+Z= np.zeros(10)
+
+Z
+
+Z= np.zeros((10, 10))
+Z
+
+O= np.ones((10, 10))
+O
+
+R = np.random.random((10,10))
+
+R
+
+G = np.random.randn(10,10)
+
+G
+
+G.mean()
+G.var()
+
+A = np.array([[1, 2], [3,4]])
+
+Ainv = np.linalg.inv(A)
+
+Ainv
+
+Ainv.dot(A)
+
+A.dot(Ainv)
+
+np.linalg.det(A)
+
+np.diag(A)
+
+np.diag([1,2])
+
+A = np.array([1, 2])
+
+B = np.array( [3,4])
+
+np.outer(A, B)
+
+np.inner(A,B)
+
+A.dot(B)
+
+np.diag(A).sum()
+
+np.trace(A)
+
+X = np.random.randn(100,3)
+
+cov = np.cov(X.T)
+
+cov.shape
+cov
+
+np.linalg.eigh(cov)
+
+np.linalg.eig(cov)
+
+
+
+
+
+
+
+
