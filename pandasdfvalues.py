65fe33325ef3c17c103e19152f79d871693de388
twheelertech <tony@wheelertechconsulting.com>
2019-09-03 03:45:01 -0500

    values

---
+++ pandasdfvalues.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 03:38:03 2019
+
+@author: tony
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.DataFrame([[1,2], [3,4]]) 
+
+df.as_matrix()
+
+df.values
